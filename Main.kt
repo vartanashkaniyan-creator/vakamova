042a84f2bbe06258a3a3067985c23e04be7c9044
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-26 17:19:20 +0330

    Update Main.kt
    Signed-off-by: vartanashkaniyan-creator <vartanashkaniyan@gmail.com>

--- Main.kt
+++ Main.kt
@@ -1,1 +1,16 @@
+class MainApp : Application() {
 
+    companion object {
+        lateinit var ctx: Context
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        ctx = applicationContext
+
+        Lang.init(ctx)
+        Usr.load(ctx)
+        Prg.load(ctx)
+    }
+}
