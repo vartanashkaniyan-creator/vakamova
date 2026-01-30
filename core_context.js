8fa72d2f13a605148114b4148504e922804e4a10
vartanashkaniyan-creator <vartanashkaniyan@gmail.com>
2026-01-31 00:56:57 +0330

    Update core_context.js
    Signed-off-by: vartanashkaniyan-creator <vartanashkaniyan@gmail.com>

--- core_context.js
+++ core_context.js
@@ -1,6 +1,1 @@
-const services = new Map();
-export const context = {
-    register: (name, service) => services.set(name, service),
-    get: (name) => services.get(name)
-};
-console.log('Context loaded');
+
